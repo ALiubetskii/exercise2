97a3415bf3cf7add14c07ef5aca3c9d52869cefb
ext.alyubetskiy <ext.alyubetskiy@stash.tcsbank.ru>
2018-03-30 15:34:11 +0300

    Initial commit

---
+++ src/Array.java
@@ -0,0 +1,23 @@
+public class Array {
+
+    public void initializationArray() {
+
+        int[] numbers = new int[20];
+
+
+        for (int i = 1; i < 20; i++) {
+            numbers[i] = i;
+            }
+
+        for (int i = 1; i < 20; i++) {
+            if (numbers[i] % 2 != 0) {
+                System.out.print(numbers[i]);
+            }
+            else
+                System.out.print(", ");
+            }
+        }
+
+    public
+}
+
---
+++ src/Output.java
@@ -0,0 +1,9 @@
+public class Output {
+
+    public static void main(String[] args){
+
+        Array array = new Array();
+        array.initializationArray();
+
+    }
+}
